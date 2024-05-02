13051ccaf2c8ea53ed74ec30a056d5d721c4e3c1
PollitoTactico <jose.sanchez.ortega@udla.edu.ec>
2024-05-02 07:28:11 -0500

    Se creo los modelos y se implemento la relacion entre la clase JSanchez(estudiante) y Carrera ademas de agregar algunas anotaciones se seguira actualizado...

---
+++ ExamenP1JoseSanchez/Models/Carrera.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamenP1JoseSanchez.Models
+{
+    public class Carrera
+    {
+        [Key]
+        public int IdCarrera { get; set; }
+        [Required]
+        public string? NombreCarrera { get; set; }
+        public string? Campus {  get; set; }
+        public int NumSemestre { get; set; }
+
+    }
+}
---
+++ ExamenP1JoseSanchez/Models/JSanchez.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExamenP1JoseSanchez.Models
+{
+    public class JSanchez
+    {
+        [Key]
+        public int IdSanchez {  get; set; }
+        [Required]
+        public string? Nombre { get; set; }
+        [Display(Name = "Es de la Ulda")]
+        public bool EsdelaUdla { get; set; }
+       
+        public DateOnly FechaNacimeinto { get; set; }
+        [ForeignKey("CarreraIdCarrera")]
+        public int CarreraIdCarrera { get; set; }
+        public Carrera? Carrera { get; set; }
+
+    }
+}
